ade5464c0a198c0f32d60485c57095674bfea909
Leonardo Omaña <leotangram@gmail.com>
2021-06-10 15:49:08 -0500

    Fab buttons

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -7,9 +7,20 @@ const CounterScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Counter: {counter}</Text>
-      <TouchableOpacity onPress={() => setCounter(counter => counter + 1)}>
-        <View style={styles.buttonIncrement}>
-          <Text>+1</Text>
+      <TouchableOpacity
+        onPress={() => setCounter(counter => counter + 1)}
+        style={styles.fabLocationBR}
+      >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setCounter(counter => counter - 1)}
+        style={styles.fabLocationBL}
+      >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -17,9 +28,38 @@ const CounterScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center' },
-  title: { fontSize: 40, textAlign: 'center', top: -15 },
-  buttonIncrement: { backgroundColor: 'red', borderRadius: 100 }
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    top: -15
+  },
+  fab: {
+    alignItems: 'center',
+    backgroundColor: '#5856d6',
+    borderRadius: 100,
+    height: 60,
+    justifyContent: 'center',
+    width: 60
+  },
+  fabText: {
+    color: '#ffffff',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  fabLocationBR: {
+    bottom: 25,
+    position: 'absolute',
+    right: 25
+  },
+  fabLocationBL: {
+    bottom: 25,
+    position: 'absolute',
+    left: 25
+  }
 })
 
 export default CounterScreen
