2b5c6f1485de63f4fffeca420976168f8781f1ba
Leonardo Omaña <leotangram@gmail.com>
2021-06-14 16:36:02 -0500

    Specific platform code

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { TouchableNativeFeedback, View, Text, StyleSheet, TouchableNativeFeedbackBase } from 'react-native';
+import { TouchableNativeFeedback, View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 
 interface FabProps {
     position?: 'br' | 'bl'
@@ -8,19 +8,25 @@ interface FabProps {
 }
 
 const Fab: FC<FabProps> = ({ onPress, position = 'br', title }) => {
-    return (
-        <View style={[styles.fabLocation, position === 'bl' ? styles.left : styles.right]}>
-
-            <TouchableNativeFeedback
-                background={TouchableNativeFeedback.Ripple('#28425b', false, 30)}
-                onPress={onPress}
-            >
-                <View style={styles.fab}>
-                    <Text style={styles.fabText}>{title}</Text>
-                </View>
-            </TouchableNativeFeedback>
+    const ios = () => <TouchableOpacity activeOpacity={0.8} style={[styles.fabLocation, position === 'bl' ? styles.left : styles.right]} onPress={onPress}>
+        <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
         </View>
-    )
+    </TouchableOpacity>
+
+    const android = () => <View style={[styles.fabLocation, position === 'bl' ? styles.left : styles.right]}>
+
+        <TouchableNativeFeedback
+            background={TouchableNativeFeedback.Ripple('#28425b', false, 30)}
+            onPress={onPress}
+        >
+            <View style={styles.fab}>
+                <Text style={styles.fabText}>{title}</Text>
+            </View>
+        </TouchableNativeFeedback>
+    </View>
+
+    return Platform.OS === 'ios' ? ios() : android()
 }
 
 const styles = StyleSheet.create({
